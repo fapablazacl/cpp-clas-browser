8d4c5257db1784bac95ac2a6e4fbb86a71b1d0bf
Felipe Apablaza <ing.apablaza@gmail.com>
2020-01-25 13:33:12 -0300

    Fixed link issue with a custom build script

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+/*
+#include <gtkmm.h>
 
-#include <iostream>
+int main(int argc, char *argv[]) {
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
-int main() {
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    return app->run(window);
+}
+*/
+
+#include <clang-c/Index.h>
+
+int main(int argc, char *argv[]) {
+    CXIndex index = clang_createIndex(0, 0);
     
     return 0;
 }
